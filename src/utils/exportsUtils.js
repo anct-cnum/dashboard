e9553de4003c7e54ceaa0e51524448e87eadbb6a
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-08-22 16:11:31 +0200

    creation fonction download file

---
+++ src/utils/exportsUtils.js
@@ -0,0 +1,9 @@
+export function downloadFile(exports) {
+  const url = window.URL.createObjectURL(new Blob(['\ufeff', exports?.blob], { type: 'text/plain' })); //ufeff pour BOM UTF-8
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', `${exports?.nameFile}.csv`);
+  document.body.appendChild(link);
+  link.click();
+  link.parentNode.removeChild(link);
+}
--- src/views/connected/structure/Exports.js
+++ src/views/connected/structure/Exports.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { exportsActions } from '../../../actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Oval } from 'react-loader-spinner';
+import { downloadFile } from '../../../utils/exportsUtils';
 
 function Exports() {
 
@@ -11,16 +12,9 @@ function Exports() {
 
   useEffect(() => {
     if (exports?.blob !== null && exports?.blob !== undefined && (error === undefined || error === false)) {
-      const url = window.URL.createObjectURL(new Blob(['\ufeff', exports?.blob], { type: 'text/plain' })); //ufeff pour BOM UTF-8
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `${exports?.nameFile}.csv`);
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
-      dispatch(exportsActions.resetFile()); //nécessaire pour ne pas reconstruire le fichier au rechargement de la page
+      downloadFile(exports);
+      dispatch(exportsActions.resetFile());
     }
-
   }, [exports]);
 
   const getFile = nameFile => {
