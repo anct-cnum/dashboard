3ffe938a1ff3e8d231da8cb0353d7049dd7018b3
Jean-Yves Stervinou <jy@stervinou.com>
2022-10-04 17:38:24 +0200

    verifyCandidateToken n'est plus utilisé

--- src/actions/conseiller.actions.js
+++ src/actions/conseiller.actions.js
@@ -10,7 +10,6 @@ export const conseillerActions = {
   updateDateRupture,
   updateMotifRupture,
   preSelectionner,
-  verifyCandidateToken,
   getCurriculumVitae,
   getStructureByIdConseiller
 };
@@ -213,32 +212,6 @@ function preSelectionner({ conseillerId, structureId }) {
     return { type: 'PRESELECTIONNER_CONSEILLER_FAILURE', error };
   }
 
-}
-
-function verifyCandidateToken(token) {
-  return dispatch => {
-    dispatch(request(token));
-
-    conseillerService.verifyCandidateToken(token)
-    .then(
-      result => {
-        dispatch(success(result.isValid, result.conseiller));
-      },
-      error => {
-        dispatch(failure(error));
-      }
-    );
-  };
-
-  function request(token) {
-    return { type: 'VERIFY_CANDIDATE_TOKEN_REQUEST', token };
-  }
-  function success(isValid, conseiller) {
-    return { type: 'VERIFY_CANDIDATE_TOKEN_SUCCESS', isValid, conseiller };
-  }
-  function failure(error) {
-    return { type: 'VERIFY_CANDIDATE_TOKEN_FAILURE', error };
-  }
 }
 
 function getCurriculumVitae(id, candidat) {
