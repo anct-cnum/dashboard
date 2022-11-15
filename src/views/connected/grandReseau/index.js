0e88fd4a9a1a8713e31ca01a5bc055caba780af2
Sébastien Quirrion <sebastien@fitlab.fr>
2022-11-15 15:44:17 +0100

    Ajout message accueil et precisions roles

--- src/views/connected/grandReseau/index.js
+++ src/views/connected/grandReseau/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import MesInformations from './MesInformations';
 import ConseillerDetails from './ConseillerDetails';
 import TableauStructures from './structures/TableauStructures';
@@ -7,8 +7,19 @@ import StructureDetails from './structures/StructureDetails';
 
 export default function GrandReseau() {
 
+  const location = useLocation();
+
   return (
     <>
+      { location.pathname === '/accueil' &&
+        <div className="fr-grid-row fr-grid-row--center fr-my-15w">
+          <div className="fr-col--offset-1 fr-col-10">
+            <h3>Bienvenue sur votre tableau de pilotage,</h3>
+            <p>Cet espace vous permet de visualiser l&rsquo;ensemble des informations des CnFS que vous coordonnez ainsi que celles de leur structure,
+              de t&eacute;l&eacute;charger les donn&eacute;es statistiques des CnFS ou encore consulter toute la documentation li&eacute;e au dispositif.</p>
+          </div>
+        </div>
+      }
       <Routes>
         <Route path="/grandReseau/informations" element={<MesInformations />} />
         <Route path="/grandReseau/liste-structures" element={<TableauStructures />} />
