1cb104678bcbc1bf6a405c2eababcf10bbbce55c
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-02-06 17:53:45 +0100

    fix erreur sentry

--- src/views/connected/coordinateur/MesInformations.js
+++ src/views/connected/coordinateur/MesInformations.js
@@ -11,7 +11,7 @@ function MesInformations() {
 
 
   useEffect(() => {
-    dispatch(conseillerActions.get(userAuth?.entity.$id));
+    dispatch(conseillerActions.get(userAuth?.entity?.$id));
   }, []);
 
   return (
