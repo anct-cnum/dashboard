c310b75b3d7ee183acd01bcb42937dd17f1ed796
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-03-17 17:29:26 +0200

    changement appel routes

--- src/services/conventionService.js
+++ src/services/conventionService.js
@@ -9,7 +9,7 @@ export const conventionService = {
 };
 
 function getAll(page, typeConvention) {
-  return API.get(`${apiUrlRoot}/reconventionnements?role=${roleActivated()}&page=${page}&type=${typeConvention}`)
+  return API.get(`${apiUrlRoot}/conventions?role=${roleActivated()}&page=${page}&type=${typeConvention}`)
   .then(response => response.data)
   .catch(error => Promise.reject(error.response.data.message));
 }
@@ -18,13 +18,13 @@ function getAllHistorique(page, typeConvention, dateDebut, dateFin) {
   const filterDateStart = (dateDebut !== '') ? `&dateDebut=${new Date(dateDebut).toISOString()}` : '';
   const filterDateEnd = (dateFin !== '') ? `&dateFin=${new Date(dateFin).toISOString()}` : '';
   // eslint-disable-next-line max-len
-  return API.get(`${apiUrlRoot}/historique/reconventionnements?role=${roleActivated()}&page=${page}&type=${typeConvention}${filterDateStart}${filterDateEnd}`)
+  return API.get(`${apiUrlRoot}/historique/conventions?role=${roleActivated()}&page=${page}&type=${typeConvention}${filterDateStart}${filterDateEnd}`)
   .then(response => response.data)
   .catch(error => Promise.reject(error.response.data.message));
 }
 
 function get(id) {
-  return API.get(`${apiUrlRoot}/reconventionnement/${id}?role=${roleActivated()}`)
+  return API.get(`${apiUrlRoot}/convention/${id}?role=${roleActivated()}`)
   .then(response => response.data)
   .catch(error => Promise.reject(error.response.data.message));
 }
