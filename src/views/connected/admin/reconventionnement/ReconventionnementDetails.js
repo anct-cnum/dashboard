23d103ff2ed268f257bb02b8208a97204c41436e
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-06-09 13:54:47 +0200

    fix: oubli type historique + changement array conseillers conven phase 1

--- src/views/connected/admin/reconventionnement/ReconventionnementDetails.js
+++ src/views/connected/admin/reconventionnement/ReconventionnementDetails.js
@@ -202,7 +202,7 @@ function ReconventionnementDetails({ reconventionnement }) {
               <div className="fr-col-12">
                 <h6 className="fr-card__desc fr-h6">Profils recrut&eacute;s</h6>
               </div>
-              {reconventionnement?.conseillers?.map((conseiller, index) =>
+              {reconventionnement?.conseillersRecruter?.map((conseiller, index) =>
                 <div key={index} className="fr-card fr-col-12 fr-mt-3w fr-p-3w">
                   <div className="fr-card__body fr-p-0">
                     <div className="fr-grid-row" style={{ alignItems: 'center' }}>
