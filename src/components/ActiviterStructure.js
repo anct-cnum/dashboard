c44d542778063e0a7baacc7e4bd58578abc500c5
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-06-22 19:26:51 +0200

    feat: ajout conventionnement phase 2 dans le détail d'une structure

---
+++ src/components/ActiviterStructure.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { formatNomConseiller, pluralize } from '../utils/formatagesUtils';
+
+function ActiviterStructure({ structure, roleActivated }) {
+
+  return (
+    <>
+      <div className="fr-grid-row fr-mt-6w fr-mb-4w fr-col-12">
+        <div className="fr-col-12 titreCol">
+          <h1>Activit&eacute;</h1>
+        </div>
+      </div>
+      <div className="fr-grid-row fr-col-12">
+        <div className="fr-col-6">
+          <h4 className="titre">Conventionnement phase 1</h4>
+          <div className="fr-mb-3w">
+            <strong>{pluralize(
+              'Postes validé en comité de sélection',
+              'Postes validé en comité de sélection',
+              'Postes validés en comité de sélection',
+              structure?.posteValiderCoselecConventionnement
+            )}</strong><br />
+            <span>{structure?.posteValiderCoselecConventionnement ?? '-'}</span>
+          </div>
+          <div className="fr-mb-3w fr-grid-row">
+            <strong>{pluralize(
+              'Profil recruté',
+              'Profil recruté',
+              'Profils recrutés',
+              structure?.conseillersRecruterConventionnement?.length
+            )}</strong>
+            {structure?.conseillersRecruterConventionnement?.map(conseiller =>
+              <span key={conseiller._id} className="fr-col-12" style={{ height: '2rem' }}>
+                <button
+                  style={{ paddingLeft: '0' }}
+                  title="D&eacute;tail"
+                  className="fr-text--md"
+                  onClick={() => window.open(`/${roleActivated}/conseiller/${conseiller?._id}`)}>
+                  {conseiller?.idPG}&nbsp;-&nbsp;{formatNomConseiller(conseiller)}
+                </button>
+              </span>
+            )}
+            {structure?.conseillersRecruterConventionnement?.length === 0 &&
+              <span className="fr-col-12">-</span>
+            }
+          </div>
+          <div className="fr-mb-3w fr-grid-row">
+            <strong>{pluralize(
+              'Profil validé',
+              'Profil validé',
+              'Profils validés',
+              structure?.conseillersValiderConventionnement?.length
+            )}</strong>
+            {structure?.conseillersValiderConventionnement?.map(conseiller =>
+              <span key={conseiller._id} className="fr-col-12" style={{ height: '2rem' }}>
+                <button
+                  style={{ paddingLeft: '0' }}
+                  title="D&eacute;tail"
+                  className="fr-text--md"
+                  onClick={() => window.open(`/${roleActivated}/candidat/${conseiller?._id}`)}>
+                  {conseiller?.idPG}&nbsp;-&nbsp;{conseiller ? formatNomConseiller(conseiller) : ''}
+                </button>
+              </span>
+            )}
+            {structure?.conseillersValiderConventionnement?.length === 0 &&
+              <span className="fr-col-12">-</span>
+            }
+          </div>
+        </div>
+        <div className="fr-col-6">
+          <h4 className="titre">Conventionnement phase 2</h4>
+          <div className="fr-mb-3w">
+            <strong>{pluralize(
+              'Postes validé en comité de sélection',
+              'Postes validé en comité de sélection',
+              'Postes validés en comité de sélection',
+              structure?.posteValiderCoselec
+            )}</strong><br />
+            <span>{structure?.posteValiderCoselec ?? '-'}</span>
+          </div>
+          <div className="fr-mb-3w fr-grid-row">
+            <strong>{pluralize(
+              'Profil recruté',
+              'Profil recruté',
+              'Profils recrutés',
+              structure?.conseillersRecruterReconventionnement?.length
+            )}</strong>
+            {structure?.conseillersRecruterReconventionnement?.map(conseiller =>
+              <span key={conseiller._id} className="fr-col-12" style={{ height: '2rem' }}>
+                <button
+                  style={{ paddingLeft: '0' }}
+                  title="D&eacute;tail"
+                  className="fr-text--md"
+                  onClick={() => window.open(`/${roleActivated}/conseiller/${conseiller?._id}`)}>
+                  {conseiller?.idPG}&nbsp;-&nbsp;{formatNomConseiller(conseiller)}
+                </button>
+              </span>
+            )}
+            {structure?.conseillersRecruterReconventionnement?.length === 0 &&
+              <span className="fr-col-12">-</span>
+            }
+          </div>
+          <div className="fr-mb-3w fr-grid-row">
+            <strong>{pluralize(
+              'Profil validé',
+              'Profil validé',
+              'Profils validés',
+              structure?.conseillersValiderReconventionnement?.length
+            )}</strong>
+            {structure?.conseillersValiderReconventionnement?.map(conseiller =>
+              <span key={conseiller._id} className="fr-col-12" style={{ height: '2rem' }}>
+                <button
+                  style={{ paddingLeft: '0' }}
+                  title="D&eacute;tail"
+                  className="fr-text--md"
+                  onClick={() => window.open(`/${roleActivated}/candidat/${conseiller?._id}`)}>
+                  {conseiller?.idPG}&nbsp;-&nbsp;{conseiller ? formatNomConseiller(conseiller) : ''}
+                </button>
+              </span>
+            )}
+            {structure?.conseillersValiderReconventionnement?.length === 0 &&
+              <span className="fr-col-12">-</span>
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+ActiviterStructure.propTypes = {
+  structure: PropTypes.object,
+  roleActivated: PropTypes.string
+};
+
+export default ActiviterStructure;
