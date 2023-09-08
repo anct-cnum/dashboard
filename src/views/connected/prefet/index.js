d1418b81f8d62c3fdc4c23432c08bc383e6b0e8b
morgan <morgan@fitlab.fr>
2023-09-08 17:19:48 +0200

    Wording de l'accueil du préfet

--- src/views/connected/prefet/index.js
+++ src/views/connected/prefet/index.js
@@ -23,9 +23,24 @@ export default function Prefet() {
         <div className="fr-grid-row fr-grid-row--center fr-my-15w">
           <div className="fr-col--offset-1 fr-col-10">
             <h3>Bienvenue sur votre tableau de pilotage,</h3>
-            <p>Cet espace vous permet de visualiser l&rsquo;ensemble des structures qui ont manifest&eacute;s leur int&eacute;r&ecirc;t pour le dispositif
-              Conseiller Num&eacute;rique France Services sur votre d&eacute;partement et r&eacute;gion, suivre les statistiques d&rsquo;usages des CnFS
-              sur votre territoire ou encore consulter toute la documentation li&eacute;e au dispositif.</p>
+            <p>Nous sommes ravis de vous accueillir sur votre nouvel espace de pilotage.</p>
+            <p>Pour g&eacute;rer au mieux le dispositif Conseiller num&eacute;rique au sein de votre territoire, vous avez besoin d&rsquo;indicateurs et
+              d&rsquo;informations cl&eacute;s pour suivre et piloter l&rsquo;activit&eacute; des structures, des conseillers num&eacute;rique et des
+              conseillers num&eacute;rique coordinateurs. C&rsquo;est pourquoi votre espace change et &eacute;volue !</p>
+            <p> Il permet de&nbsp;:
+              <ul>
+                <li>Visualiser le liste des structures laur&eacute;ates de votre territoire</li>
+                <li>Visualiser la liste des conseillers num&eacute;rique exerçant sur votre territoire</li>
+                <li>Consulter le formulaire de motivation d&rsquo;une structure candidate au poste de coordinateur</li>
+                <li>Donner un avis sur une candidature de coordinateur</li>
+                <li>D&rsquo;acc&eacute;der aux informations g&eacute;n&eacute;rales d&rsquo;une structure et d&rsquo;un conseiller</li>
+                <li>Visualiser les statistiques des conseillers et celles des structures</li>
+              </ul>
+              Et bien plus encore...
+            </p>
+            <p><b>Vous avez des questions ?</b></p>
+            <p>L&rsquo;&eacute;quipe d&rsquo;animation se tient &agrave; votre disposition par email &agrave; l&rsquo;adresse&nbsp;:&nbsp;
+              <a className="email" href={'mailto:conseiller-numerique@anct.gouv.fr'}>conseiller-numerique@anct.gouv.fr</a></p>
           </div>
         </div>
       }
