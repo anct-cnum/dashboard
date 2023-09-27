306d63d83de9e88a9824e55c0c40ad3e2964e5ec
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-09-27 14:24:54 +0200

    fix: entities

--- src/views/connected/admin/coordinateurs/ModalConfirmationAttributionPoste.js
+++ src/views/connected/admin/coordinateurs/ModalConfirmationAttributionPoste.js
@@ -30,7 +30,7 @@ function ModalConfirmationAttributionPoste({ setOpenModal, structure, typeAttrib
                 </h1>
                 <p>
                   Souhaitez-vous {typeAttribution} l&rsquo;attribution d&rsquo;un poste de coordinateur
-                  à la structure&nbsp;<strong>{structure?.nom}</strong>&nbsp;?
+                  &agrave; la structure&nbsp;<strong>{structure?.nom}</strong>&nbsp;?
                 </p>
               </div>
               <div className="fr-modal__footer">
