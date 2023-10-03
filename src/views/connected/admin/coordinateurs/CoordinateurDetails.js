1c7d553cec542d06706962e2c987e31de143e360
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-10-03 09:19:11 +0200

    fix: Sentry Objects are not valid as a React child (found: [object Error])

--- src/views/connected/admin/coordinateurs/CoordinateurDetails.js
+++ src/views/connected/admin/coordinateurs/CoordinateurDetails.js
@@ -54,91 +54,95 @@ function CoordinateurDetails() {
       {openModalAttributionPoste &&
         <ModalConfirmationAttributionPoste setOpenModal={setOpenModalAttributionPoste} structure={structure} typeAttribution={typeAttribution} />
       }
-      <div className="fr-col-12 fr-pt-6w">
-        <h1 className="fr-h1 fr-mb-1w" style={{ color: '#000091' }}>{structure?.nom ?? '-'}</h1>
-      </div>
-      <div className="fr-col-12 fr-mb-4w">
-        <div className="fr-grid-row" style={{ alignItems: 'center' }}>
-          <span className="fr-text--xl fr-text--bold" style={{ marginBottom: '0' }}>ID - {structure?.idPG}</span>
-          <button className="fr-btn fr-icon-eye-line fr-btn--icon-left fr-ml-auto"
-            onClick={() => window.open(`/${roleActivated}/structure/${structure?._id}`)}>
-            D&eacute;tails structure
-          </button>
-        </div>
-      </div>
-      <StructureContactCards structure={structure} />
-      <div className="fr-grid-row fr-mt-7w fr-mb-2w fr-col-12">
-        <div className="fr-col-12">
-          <hr style={{ borderWidth: '0.5px' }} />
-        </div>
-      </div>
-      <h2>Candidature</h2>
-      <div className="fr-card">
-        <div className="fr-card__body">
-          <div className="fr-card__header fr-mt-4w">
-            <h3 className="fr-card__title fr-h3">
-              Recrutement coordinateur
-            </h3>
-            {structure?.demandesCoordinateur[0]?.avisPrefet === 'favorable' ?
-              <p className="fr-badge fr-badge--success badge-avis-prefet">Avis pr&eacute;fet favorable</p> :
-              <p className="fr-badge fr-badge--error badge-avis-prefet">Avis pr&eacute;fet d&eacute;favorable</p>
-            }
-            <p className="fr-card__desc fr-text--lg fr-text--regular">
-              Date de candidature&nbsp;:&nbsp;
-              {structure?.demandesCoordinateur[0]?.dossier?.dateDeCreation ?
-                <span>le&nbsp;{dayjs(structure?.demandesCoordinateur[0].dossier.dateDeCreation).format('DD/MM/YYYY')}</span> :
-                <span>Non renseign&eacute;e</span>
-              }
-            </p>
+      {structure ?
+        <>
+          <div className="fr-col-12 fr-pt-6w">
+            <h1 className="fr-h1 fr-mb-1w" style={{ color: '#000091' }}>{structure?.nom ?? '-'}</h1>
           </div>
-          <div className="fr-card__content">
-            <div className="commentaire-prefet">
-              <span><strong>Commentaire pr&eacute;fet&nbsp;:&nbsp;</strong></span>
-              <p className="fr-mt-2w fr-mb-0">{structure?.demandesCoordinateur[0]?.commentaire ?? 'Non renseigné'}</p>
+          <div className="fr-col-12 fr-mb-4w">
+            <div className="fr-grid-row" style={{ alignItems: 'center' }}>
+              <span className="fr-text--xl fr-text--bold" style={{ marginBottom: '0' }}>ID - {structure?.idPG}</span>
+              <button className="fr-btn fr-icon-eye-line fr-btn--icon-left fr-ml-auto"
+                onClick={() => window.open(`/${roleActivated}/structure/${structure?._id}`)}>
+                D&eacute;tails structure
+              </button>
             </div>
-            <div className="fr-container questionnaire">
-              <h6 className="fr-text--bold fr-mb-4w">R&eacute;ponses au questionnaire D&eacute;marches-Simplifi&eacute;es</h6>
-              {structure?.questionnaire?.map((question, idx) =>
-                <div key={idx}>
-                  <p className="fr-text--bold">{question.enoncer}</p>
-                  {question.files?.length > 0 ?
-                    question.files?.map((file, idx) =>
-                      <div key={idx} className="fr-mb-4w">
-                        <a href={file?.url} target="_blank" rel="noopener noreferrer">{file?.filename}</a>
-                      </div>
-                    ) :
-                    <p>{question.reponse}</p>
+          </div>
+          <StructureContactCards structure={structure} />
+          <div className="fr-grid-row fr-mt-7w fr-mb-2w fr-col-12">
+            <div className="fr-col-12">
+              <hr style={{ borderWidth: '0.5px' }} />
+            </div>
+          </div>
+          <h2>Candidature</h2>
+          <div className="fr-card">
+            <div className="fr-card__body">
+              <div className="fr-card__header fr-mt-4w">
+                <h3 className="fr-card__title fr-h3">
+                  Recrutement coordinateur
+                </h3>
+                {structure?.demandesCoordinateur[0]?.avisPrefet === 'favorable' ?
+                  <p className="fr-badge fr-badge--success badge-avis-prefet">Avis pr&eacute;fet favorable</p> :
+                  <p className="fr-badge fr-badge--error badge-avis-prefet">Avis pr&eacute;fet d&eacute;favorable</p>
+                }
+                <p className="fr-card__desc fr-text--lg fr-text--regular">
+                  Date de candidature&nbsp;:&nbsp;
+                  {structure?.demandesCoordinateur[0]?.dossier?.dateDeCreation ?
+                    <span>le&nbsp;{dayjs(structure?.demandesCoordinateur[0].dossier.dateDeCreation).format('DD/MM/YYYY')}</span> :
+                    <span>Non renseign&eacute;e</span>
                   }
-                  {idx + 1 < structure?.questionnaire?.length &&
-                    <hr />
-                  }
+                </p>
+              </div>
+              <div className="fr-card__content">
+                <div className="commentaire-prefet">
+                  <span><strong>Commentaire pr&eacute;fet&nbsp;:&nbsp;</strong></span>
+                  <p className="fr-mt-2w fr-mb-0">{structure?.demandesCoordinateur[0]?.commentaire ?? 'Non renseigné'}</p>
                 </div>
-              )}
+                <div className="fr-container questionnaire">
+                  <h6 className="fr-text--bold fr-mb-4w">R&eacute;ponses au questionnaire D&eacute;marches-Simplifi&eacute;es</h6>
+                  {structure?.questionnaire?.map((question, idx) =>
+                    <div key={idx}>
+                      <p className="fr-text--bold">{question.enoncer}</p>
+                      {question.files?.length > 0 ?
+                        question.files?.map((file, idx) =>
+                          <div key={idx} className="fr-mb-4w">
+                            <a href={file?.url} target="_blank" rel="noopener noreferrer">{file?.filename}</a>
+                          </div>
+                        ) :
+                        <p>{question.reponse}</p>
+                      }
+                      {idx + 1 < structure?.questionnaire?.length &&
+                        <hr />
+                      }
+                    </div>
+                  )}
+                </div>
+              </div>
+              {structure?.demandesCoordinateur[0]?.statut === 'en_cours' &&
+                <div className="fr-card__footer">
+                  <ul className="fr-btns-group fr-btns-group--right fr-btns-group--inline-lg">
+                    <li>
+                      <button onClick={() => {
+                        setTypeAttribution('refuser');
+                        setOpenModalAttributionPoste(true);
+                      }}
+                      className="fr-btn fr-btn--secondary"
+                      >
+                        Refuser la candidature
+                      </button>
+                    </li>
+                    <li>
+                      <button className="fr-btn">
+                        Valider la candidature
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              }
             </div>
           </div>
-          {structure?.demandesCoordinateur[0]?.statut === 'en_cours' &&
-            <div className="fr-card__footer">
-              <ul className="fr-btns-group fr-btns-group--right fr-btns-group--inline-lg">
-                <li>
-                  <button onClick={() => {
-                    setTypeAttribution('refuser');
-                    setOpenModalAttributionPoste(true);
-                  }}
-                  className="fr-btn fr-btn--secondary"
-                  >
-                    Refuser la candidature
-                  </button>
-                </li>
-                <li>
-                  <button className="fr-btn">
-                    Valider la candidature
-                  </button>
-                </li>
-              </ul>
-            </div>
-          }
-        </div>
-      </div>
+        </> : <></>
+      }
     </div>
   );
 }
--- src/views/connected/structure/MaStructure.js
+++ src/views/connected/structure/MaStructure.js
@@ -105,116 +105,124 @@ function MaStructure() {
       )}
       <div className="fr-container maStructure">
         {deleteMessageSuccess &&
-        <div className="fr-alert fr-alert--success" style={{ marginBottom: '2rem' }}>
-          <p className="fr-alert__title">
-            {deleteMessageSuccess}
-          </p>
-        </div>
+          <div className="fr-alert fr-alert--success" style={{ marginBottom: '2rem' }}>
+            <p className="fr-alert__title">
+              {deleteMessageSuccess}
+            </p>
+          </div>
         }
         <Spinner loading={loadingStructure || loadingInvitation || loadingSuppression} />
-        <h2 className="fr-mb-1w titre">
-          {structure?.nom}
-        </h2>
-        <h6>{`ID - ${structure?.idPG}`}</h6>
-        <StructureInformationsCard structure={structure} formInformationContact={formInformationContact} setFormInformationContact={setFormInformationContact}/>
-        <div className="fr-col-12 fr-mb-2w fr-mt-7w">
-          <hr style={{ borderWidth: '0.5px' }} />
-        </div>
-        <h2>Activit&eacute;</h2>
-        {structure?.conventionnement?.statut && (structure?.conventionnement?.statut === StatutConventionnement.RECONVENTIONNEMENT_VALIDÉ ||
-        structure?.conventionnement?.statut === StatutConventionnement.RECONVENTIONNEMENT_EN_COURS) &&
-        (
-          <ReconventionnementInfosCard structure={structure}/>
-        )}
-        <ConventionnementInfosCard structure={structure} roleActivated={roleActivated}/>
-        <h2>Accompagnements</h2>
-        <AccompagnementsCard structure={structure} />
-        <div className="fr-col-12 fr-my-6w">
-          <hr style={{ borderWidth: '0.5px' }} />
-        </div>
-        <h2>Informations structure</h2>
-        <div className="fr-grid-row color-text color-title-subpart">
-          <div className="fr-col-md-6 fr-col-12 fr-mt-4w">
-            <h4 className="titre">
-              Informations g&eacute;n&eacute;rales
-            </h4>
-            <div className="fr-mb-3w">
-              <strong>Siret</strong>
-              <br />
-              {displaySiretForm === true ? (
-                <div style={{ width: '320px' }}>
-                  <SiretForm
-                    setDisplaySiretForm={setDisplaySiretForm}
-                    structureId={structure?._id}
-                    structureSiret={structure?.siret}
-                  />
-                </div>
-              ) : (
-                <div>
-                  <span>{structure?.siret ?? '-'}</span>
-                  <button onClick={() => setDisplaySiretForm(true)} className="fr-grid fr-ml-1w">
-                    <span className="fr-icon-edit-line"></span>
-                  </button>
-                </div>
+        {structure ?
+          <>
+            <h2 className="fr-mb-1w titre">
+              {structure?.nom}
+            </h2>
+            <h6>{`ID - ${structure?.idPG}`}</h6>
+            <StructureInformationsCard
+              structure={structure}
+              formInformationContact={formInformationContact}
+              setFormInformationContact={setFormInformationContact}
+            />
+            <div className="fr-col-12 fr-mb-2w fr-mt-7w">
+              <hr style={{ borderWidth: '0.5px' }} />
+            </div>
+            <h2>Activit&eacute;</h2>
+            {structure?.conventionnement?.statut && (structure?.conventionnement?.statut === StatutConventionnement.RECONVENTIONNEMENT_VALIDÉ ||
+              structure?.conventionnement?.statut === StatutConventionnement.RECONVENTIONNEMENT_EN_COURS) &&
+              (
+                <ReconventionnementInfosCard structure={structure} />
               )}
+            <ConventionnementInfosCard structure={structure} roleActivated={roleActivated} />
+            <h2>Accompagnements</h2>
+            <AccompagnementsCard structure={structure} />
+            <div className="fr-col-12 fr-my-6w">
+              <hr style={{ borderWidth: '0.5px' }} />
             </div>
-            <div className="fr-mb-3w">
-              <strong>Date d&lsquo;inscription</strong>
-              <br />
-              {structure?.createdAt ? <span>{dayjs(structure?.createdAt).format('DD/MM/YYYY')}</span> : <span>-</span>}
+            <h2>Informations structure</h2>
+            <div className="fr-grid-row color-text color-title-subpart">
+              <div className="fr-col-md-6 fr-col-12 fr-mt-4w">
+                <h4 className="titre">
+                  Informations g&eacute;n&eacute;rales
+                </h4>
+                <div className="fr-mb-3w">
+                  <strong>Siret</strong>
+                  <br />
+                  {displaySiretForm === true ? (
+                    <div style={{ width: '320px' }}>
+                      <SiretForm
+                        setDisplaySiretForm={setDisplaySiretForm}
+                        structureId={structure?._id}
+                        structureSiret={structure?.siret}
+                      />
+                    </div>
+                  ) : (
+                    <div>
+                      <span>{structure?.siret ?? '-'}</span>
+                      <button onClick={() => setDisplaySiretForm(true)} className="fr-grid fr-ml-1w">
+                        <span className="fr-icon-edit-line"></span>
+                      </button>
+                    </div>
+                  )}
+                </div>
+                <div className="fr-mb-3w">
+                  <strong>Date d&lsquo;inscription</strong>
+                  <br />
+                  {structure?.createdAt ? <span>{dayjs(structure?.createdAt).format('DD/MM/YYYY')}</span> : <span>-</span>}
+                </div>
+                <div className="fr-mb-3w">
+                  <strong>Type</strong>
+                  <br />
+                  <span>{structure?.type ?? '-'}</span>
+                </div>
+                <div className="fr-mb-3w">
+                  <strong>Quartier Prioritaire de la Ville</strong>
+                  <br />
+                  <span>{structure?.qpvStatut ?? '-'}</span>
+                </div>
+                <div className="fr-mb-3w">
+                  <strong>Zone de revitalisation rurale</strong>
+                  <br />
+                  <span>{structure?.estZRR ?? '-'}</span>
+                </div>
+              </div>
+              <div className="fr-col-md-6 fr-col-12 fr-mt-4w">
+                <h4 className="titre">
+                  Adresse
+                </h4>
+                <div className="fr-mb-3w">
+                  <strong>Adresse</strong>
+                  <br />
+                  <span>{structure?.adresseFormat ?? '-'}</span>
+                </div>
+                <div className="fr-mb-3w">
+                  <strong>Code Postal</strong>
+                  <br />
+                  <span>{structure?.codePostal ?? '-'}</span>
+                </div>
+              </div>
             </div>
-            <div className="fr-mb-3w">
-              <strong>Type</strong>
-              <br />
-              <span>{structure?.type ?? '-'}</span>
+            <div className="fr-col-12 fr-my-6w">
+              <hr style={{ borderWidth: '0.5px' }} />
             </div>
-            <div className="fr-mb-3w">
-              <strong>Quartier Prioritaire de la Ville</strong>
-              <br />
-              <span>{structure?.qpvStatut ?? '-'}</span>
+            <div className="fr-grid-row" style={{ alignItems: 'start' }}>
+              <h2>Collaborateurs</h2>
+              <button
+                className="fr-btn fr-btn--tertiary fr-btn--icon-left fr-icon-add-line fr-ml-auto"
+                onClick={() => setOpenModal(true)}
+                title="Ajouter un collaborateur"
+              >
+                Ajouter un collaborateur
+              </button>
             </div>
-            <div className="fr-mb-3w">
-              <strong>Zone de revitalisation rurale</strong>
-              <br />
-              <span>{structure?.estZRR ?? '-'}</span>
-            </div>
-          </div>
-          <div className="fr-col-md-6 fr-col-12 fr-mt-4w">
-            <h4 className="titre">
-              Adresse
-            </h4>
-            <div className="fr-mb-3w">
-              <strong>Adresse</strong>
-              <br />
-              <span>{structure?.adresseFormat ?? '-'}</span>
-            </div>
-            <div className="fr-mb-3w">
-              <strong>Code Postal</strong>
-              <br />
-              <span>{structure?.codePostal ?? '-'}</span>
-            </div>
-          </div>
-        </div>
-        <div className="fr-col-12 fr-my-6w">
-          <hr style={{ borderWidth: '0.5px' }} />
-        </div>
-        <div className="fr-grid-row" style={{ alignItems: 'start' }}>
-          <h2>Collaborateurs</h2>
-          <button
-            className="fr-btn fr-btn--tertiary fr-btn--icon-left fr-icon-add-line fr-ml-auto"
-            onClick={() => setOpenModal(true)}
-            title="Ajouter un collaborateur"
-          >
-            Ajouter un collaborateur
-          </button>
-        </div>
-        {users?.length > 0 ? (
-          users?.map((collaborateur, idx) => (
-            <CollaborateurCard gestionnaire={collaborateur} key={idx} />
-          ))
-        ) : (
-          <div className="fr-mt-3w">Aucun compte associ&eacute;.</div>
-        )}
+            {users?.length > 0 ? (
+              users?.map((collaborateur, idx) => (
+                <CollaborateurCard gestionnaire={collaborateur} key={idx} />
+              ))
+            ) : (
+              <div className="fr-mt-3w">Aucun compte associ&eacute;.</div>
+            )}
+          </> : <></>
+        }
       </div>
     </>
   );
