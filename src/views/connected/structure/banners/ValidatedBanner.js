57ff7f1c76d2bfa47de0d9b3ed4eda7d7be14987
dienamo <46058257+dienamo@users.noreply.github.com>
2023-10-06 16:03:55 +0200

    wording banniere validation reconventionnement

--- src/views/connected/structure/banners/ValidatedBanner.js
+++ src/views/connected/structure/banners/ValidatedBanner.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { pluralize } from '../../../../utils/formatagesUtils';
-import { useAdvisors } from '../hooks/useAdvisors';
 
-const ValidatedBanner = ({ structure, setShowValidateBanner }) => {
-  const {
-    conseillersRecrutes,
-    conseillersEnCoursDeRecrutement,
-  } = useAdvisors();
-
-  const postesOccupes = conseillersRecrutes?.length + conseillersEnCoursDeRecrutement?.length;
+const ValidatedBanner = ({ setShowValidateBanner }) => {
 
   function closeBanner() {
     setShowValidateBanner(false);
@@ -21,36 +13,14 @@ const ValidatedBanner = ({ structure, setShowValidateBanner }) => {
       <div className="fr-container success responsive__banner">
         <div className="fr-notice__body responsive__banner" style={{ paddingLeft: '5px' }}>
           <div>
-            <p className="fr-notice__title title__color fr-mb-2w">
+            <p className="fr-notice__title title__color">
               <span className="fr-icon-checkbox-fill icon__color fr-mr-2w" aria-hidden="true"></span>
-              Votre demande de reconventionnement a &eacute;t&eacute; accept&eacute;&nbsp;!
-            
+              Votre demande de reconventionnement a &eacute;t&eacute; prise en compte.
             </p>
-            <>
-              <p className="fr-text fr-text--sm">
-                Vous avez obtenu{' '}
-                <span className="fr-text fr-text--bold">
-                  {structure?.posteValiderCoselec}{' '}
-                  {pluralize('poste subventionné', 'poste subventionné', 'postes subventionnés', true)},{' '}
-                </span>
-                dont:
-              </p>
-              <ul className="fr-pl-4w">
-                <li>
-                  <p className="fr-text fr-text--sm">
-                    {postesOccupes} {pluralize('poste occupé', 'poste occupé', 'postes occupés', true)}
-                  </p>
-                </li>
-                <li>
-                  <p className="fr-text fr-text--sm">
-                    {structure?.posteValiderCoselec -
-                          postesOccupes}{' '}
-                    {pluralize('poste vacant', 'poste vacant', 'postes vacants', true)}
-                  </p>
-                </li>
-              </ul>
-              <p className="fr-text fr-text--sm">En savoir plus sur comment recruter vos conseillers.</p>
-            </>
+            <p className="fr-text--md" style={{ marginLeft: '41px' }}>
+             Ci-dessous, vous pouvez d&eacute;sormais proc&eacute;der &agrave; la saisie
+             des contrats de vos conseillers ou aux recrutements de vos futurs conseillers.
+            </p>
           </div>
           <div className="banner__button_progress_reconventionnement">
             <button className="fr-icon-close-line" onClick={() => closeBanner()}></button>
@@ -62,8 +32,6 @@ const ValidatedBanner = ({ structure, setShowValidateBanner }) => {
 };
 
 ValidatedBanner.propTypes = {
-  structure: PropTypes.object,
-  conseillersActifs: PropTypes.array,
   setShowValidateBanner: PropTypes.func,
 };
 
