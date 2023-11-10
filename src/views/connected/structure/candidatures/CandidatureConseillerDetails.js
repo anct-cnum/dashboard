3007d32f27d74337e42c338f473634d3902134a0
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-11-10 17:48:41 +0000

    fix: affichage modal + mauvais wording

--- src/views/connected/structure/candidatures/CandidatureConseillerDetails.js
+++ src/views/connected/structure/candidatures/CandidatureConseillerDetails.js
@@ -121,7 +121,7 @@ function CandidatureConseillerDetails() {
             <PopinInteressee setDisplayModal={setDisplayModal} />
           }
           {conseiller?.miseEnRelation?.statut === 'recrutee' &&
-            <PopinRecrutee setDisplayModal={setDisplayModal} urlDossierConventionnement={conseiller?.urlDossierDS} />
+            <PopinRecrutee setDisplayModal={setDisplayModal} urlDossierDS={conseiller?.urlDossierDS} />
           }
           {conseiller?.miseEnRelation?.statut === 'nouvelle_rupture' &&
             <PopinNouvelleRupture setDisplayModal={setDisplayModal} />
@@ -147,6 +147,7 @@ function CandidatureConseillerDetails() {
           <ButtonsAction
             miseEnRelation={conseiller?.miseEnRelation}
             updateStatut={updateStatut}
+            setDisplayModal={setDisplayModal}
           />
         </div>
       }
