87f395b6faa8557c0f1989c4868df08e7db9e88d
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-11-13 11:35:45 +0100

    feat: ajout redirection vers mail de support

--- src/views/connected/structure/banners/DeniedNewPosteCoordinateurBanner.js
+++ src/views/connected/structure/banners/DeniedNewPosteCoordinateurBanner.js
@@ -11,7 +11,7 @@ const DeniedNewPosteCoordinateurBanner = ({ idDemandeCoordinateur, idStructure }
   }
 
   return (
-    <div className="fr-notice fr-py-4w banner warning background">
+    <div className="fr-notice fr-py-4w banner warning background fr-mt-2w">
       <div className="fr-container warning responsive__banner">
         <div style={{ display: 'flex', alignItems: 'center' }} className="fr-col-12">
           <span className="fr-icon-info-fill icon__color fr-mr-2w" aria-hidden="true"></span>
@@ -20,7 +20,10 @@ const DeniedNewPosteCoordinateurBanner = ({ idDemandeCoordinateur, idStructure }
               Votre demande de poste de coordinateur a &eacute;t&eacute; refus&eacute;e.
             </p>
             <p className="fr-text fr-text--sm">
-              Pour toutes question, veuillez contacter le service support Conseiller num&eacute;rique
+              Pour toutes question, veuillez contacter le&nbsp;
+              <a style={{ color: '#000091' }} target="_blank" rel="noreferrer noopener" href="mailto:conseiller-numerique@anct.gouv.fr">
+                service support Conseiller num&eacute;rique
+              </a>
             </p>
           </div>
           <button
