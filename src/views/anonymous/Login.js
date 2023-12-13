90418283b541a66bf8fbb982fe71421cb34fa426
dienamo <46058257+dienamo@users.noreply.github.com>
2023-12-13 09:54:26 +0100

    Update src/views/anonymous/Login.js
    Co-authored-by: Sébastien Quirrion <77339872+squirrion@users.noreply.github.com>

--- src/views/anonymous/Login.js
+++ src/views/anonymous/Login.js
@@ -84,7 +84,7 @@ export default function Login() {
                 {
                   networkError && <div className="fr-alert fr-alert--error fr-mt-1w fr-mb-4w">
                     <h3 className="fr-alert__title">&Eacute;chec de la connexion &agrave; Inclusion Connect</h3>
-                    <p className="fr-mb-1v">Merci de v&eacute;rifier votre réseau ou contactez le support Inclusion Connect</p>
+                    <p className="fr-mb-1v">Merci de v&eacute;rifier votre r&eacute;seau ou contacter le support Inclusion Connect</p>
                   </div>
                 }
                 {process.env.NODE_ENV === 'development' &&
