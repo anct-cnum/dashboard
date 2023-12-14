38064166c188622eeeb596f7948f73ebbffb9311
dienamo <46058257+dienamo@users.noreply.github.com>
2023-12-14 11:07:40 +0100

    ajout de l'email du spoort IC dans le message d'erreur

--- src/views/anonymous/Login.js
+++ src/views/anonymous/Login.js
@@ -84,7 +84,9 @@ export default function Login() {
                 {
                   networkError && <div className="fr-alert fr-alert--error fr-mt-1w fr-mb-4w">
                     <h3 className="fr-alert__title">&Eacute;chec de la connexion &agrave; Inclusion Connect</h3>
-                    <p className="fr-mb-1v">Merci de v&eacute;rifier votre r&eacute;seau ou contacter le support Inclusion Connect</p>
+                    <p className="fr-mb-1v">
+                      Merci de v&eacute;rifier votre r&eacute;seau ou contacter le support Inclusion Connect par mail au support@connect.inclusion.beta.gouv.fr
+                    </p>
                   </div>
                 }
                 {process.env.NODE_ENV === 'development' &&
