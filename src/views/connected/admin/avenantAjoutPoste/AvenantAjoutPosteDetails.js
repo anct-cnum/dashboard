a58e0797326c6e30af474892269296b576297f7d
dienamo <46058257+dienamo@users.noreply.github.com>
2024-03-21 12:18:59 +0100

    modification details avenant ajout poste

--- src/views/connected/admin/avenantAjoutPoste/AvenantAjoutPosteDetails.js
+++ src/views/connected/admin/avenantAjoutPoste/AvenantAjoutPosteDetails.js
@@ -3,34 +3,67 @@ import dayjs from 'dayjs';
 import { pluralize } from '../../../../utils/formatagesUtils';
 import PropTypes from 'prop-types';
 import ModalValidationAvenantAjoutPoste from '../modals/ModalValidationAvenantAjoutPoste';
-import { useDispatch } from 'react-redux';
-import { conventionActions } from '../../../../actions';
-import { StatutCoselec } from '../../../../utils/enumUtils';
+import ModalRefusAvenantAjoutPoste from '../modals/ModalRefusAvenantAjoutPoste';
 
 function AvenantAjoutPosteDetails({ avenant, idDemandeCoselec }) {
-  const dispatch = useDispatch();
 
-  const [openModal, setOpenModal] = useState(false);
+  const [openModalValidation, setOpenModalValidation] = useState(false);
+  const [openModalRefus, setOpenModalRefus] = useState(false);
   const demandesCoselec = avenant?.demandesCoselec?.find(demande => demande.id === idDemandeCoselec);
 
-  const refusAvenantAjoutPoste = () => {
-    dispatch(conventionActions.updateAvenantAjoutPoste(avenant._id, StatutCoselec.NÉGATIF));
+
+  const formatAvisPrefet = avisPrefet => {
+    switch (avisPrefet) {
+      case 'favorable':
+        return <p className="fr-badge fr-badge--success">avis pr&eacute;fet favorable</p>;
+      case 'défavorable':
+        return <p className="fr-badge fr-badge--error">avis pr&eacute;fet d&eacute;favorable</p>;
+      default:
+        return <p className="fr-badge fr-badge--new">en attente de l&apos;avis pr&eacute;fet</p>;
+    }
   };
 
   return (
-    <div className="fr-card fr-card--no-border" style={{ backgroundColor: '#E8EDFF' }}>
-      {openModal &&
+    <div className="fr-card">
+      {openModalValidation &&
         <ModalValidationAvenantAjoutPoste
           idStructure={avenant._id}
+          nomStructure={avenant.nom}
           demandesCoselec={demandesCoselec}
           nombreConseillersCoselec={avenant.nombreConseillersCoselec}
-          setOpenModal={setOpenModal}
+          setOpenModal={setOpenModalValidation}
+        />
+      }
+      {openModalRefus &&
+        <ModalRefusAvenantAjoutPoste
+          idStructure={avenant._id}
+          nomStructure={avenant.nom}
+          demandesCoselec={demandesCoselec}
+          nombreConseillersCoselec={avenant.nombreConseillersCoselec}
+          setOpenModal={setOpenModalRefus}
         />
       }
       <div className="fr-card__body">
         <div className="fr-card__content">
           <h3 className="fr-card__title fr-h3">
-            Avenant · ajout de poste
+            {demandesCoselec?.nombreDePostesAccordes ?
+              <>
+                  Demande de {demandesCoselec?.nombreDePostesAccordes}{pluralize(
+                  ' poste supplémentaire ',
+                  ' poste supplémentaire ',
+                  ' postes de supplémentaires ',
+                  demandesCoselec?.nombreDePostesAccordes
+                )}
+              </> :
+              <>
+                  Demande de {demandesCoselec?.nombreDePostesSouhaites}{pluralize(
+                  ' poste supplémentaire ',
+                  ' poste supplémentaire ',
+                  ' postes supplémentaires ',
+                  demandesCoselec?.nombreDePostesSouhaites
+                )}
+              </>
+            }
           </h3>
           <p className="fr-card__desc fr-text--lg fr-text--regular">
             Demande initi&eacute;e&nbsp;
@@ -39,80 +72,77 @@ function AvenantAjoutPosteDetails({ avenant, idDemandeCoselec }) {
               <span>&agrave; une date inconnue</span>
             }
           </p>
-          <p className="fr-card__desc fr-text--lg" style={{ color: '#000091', width: '54%' }}>
-            <strong className="fr-text--bold">
-              {demandesCoselec?.nombreDePostesAccordes ?
-                <>
-                  Demande de {demandesCoselec?.nombreDePostesAccordes}{pluralize(
-                    ' poste de conseiller subventionné supplémentaire ',
-                    ' poste de conseiller subventionné supplémentaire ',
-                    ' postes de conseillers subventionnés supplémentaires ',
-                    demandesCoselec?.nombreDePostesAccordes
-                  )}
-                </> :
-                <>
-                  Demande de {demandesCoselec?.nombreDePostesSouhaites}{pluralize(
-                    ' poste de conseiller subventionné supplémentaire ',
-                    ' poste de conseiller subventionné supplémentaire ',
-                    ' postes de conseillers subventionnés supplémentaires ',
-                    demandesCoselec?.nombreDePostesSouhaites
-                  )}
-                </>
-              }
-            </strong>
-            en plus{pluralize(
-              ' d\'un ',
-              ' d\'un ',
-              ' des ',
-              avenant?.nombreConseillersCoselec
-            )}
-            {avenant?.nombreConseillersCoselec}
-            {pluralize(
-              ' poste de conseiller validé ',
-              ' poste de conseiller validé ',
-              ' postes de conseillers validés ',
-              avenant?.nombreConseillersCoselec
-            )}
-            pour ce conventionnement
-          </p>
-          <p className="fr-card__desc fr-text--lg">
-            Motif: {demandesCoselec?.motif ?? 'Non renseigné'}
-          </p>
+          <div className="fr-card__desc" style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <p className="fr-text--lg" style={{ color: '#000091', width: '54%' }}>
+              <strong className="fr-text--bold">
+                {demandesCoselec?.nombreDePostesAccordes &&
+                  <>
+                    {demandesCoselec?.nombreDePostesAccordes}{pluralize(
+                      ' poste attribué a cette structure',
+                      ' poste attribué a cette structure',
+                      ' postes attribués a cette structure',
+                      demandesCoselec?.nombreDePostesAccordes
+                    )}
+                  </>
+                }
+              </strong>
+            </p>
+            <a className="fr-btn fr-btn--tertiary-no-outline" href={avenant?.url} target="_blank" rel="noopener noreferrer">
+                Voir le dossier D&eacute;marche Simplifi&eacute;e
+            </a>
+          </div>
+          <hr className="fr-card__desc" style={{ marginLeft: '-2rem', marginRight: '-2rem' }}/>
+          <div className="fr-card__desc fr-container--fluid">
+            <div className=" fr-grid-row" style={{ justifyContent: 'space-between' }}>
+              <div className="fr-card fr-card--no-border custom-card-style-info">
+                <div className="fr-card__body">
+                  <div className="fr-card__content">
+                    <p className="fr-text--bold" style={{ marginBottom: '0px' }}>
+                      Motif de la structure :
+                    </p>
+                    <p className="fr-card__desc">{demandesCoselec?.motif ?? 'Non renseigné'}</p>
+                  </div>
+                </div>
+              </div>
+              {demandesCoselec.commentairePrefet &&
+              <div className={`fr-card fr-card--no-border 
+              ${demandesCoselec.avisPrefet === 'favorable' ? 'custom-card-style-accept' : 'custom-card-style-decline'}`}>
+                <div className="fr-card__body">
+                  <div className="fr-card__content">
+                    <p className="fr-text--bold" style={{ marginBottom: '0px' }}>
+                      Commentaire pr&eacute;fet :
+                    </p>
+                    <p className="fr-card__desc">{demandesCoselec?.commentairePrefet}</p>
+                    <p className={`fr-card__desc fr-text--bold 
+                    ${demandesCoselec?.avisPrefet === 'favorable' ? 'custom-card-text-accept' : 'custom-card-text-decline'}`}>
+                      {demandesCoselec.avisPrefet === 'favorable' ? 'Avis favorable' : 'Avis défavorable'}
+                    </p>
+                  </div>
+                </div>
+              </div>}
+            </div>
+          </div>
           <div className="fr-card__start fr-mb-0" style={{ textAlign: 'end' }}>
-            {demandesCoselec?.statut === 'refusee' &&
-              <p className="fr-badge fr-badge--error">Demande refus&eacute;e</p>
-            }
-            {demandesCoselec?.statut === 'validee' &&
-              <p className="fr-badge fr-badge--success">Demande valid&eacute;e</p>
-            }
-            {demandesCoselec?.statut === 'en_cours' &&
-              <p className="fr-badge fr-badge--new">Demande en attente de validation</p>
-            }
+            {formatAvisPrefet(demandesCoselec?.avisPrefet)}
           </div>
         </div>
         <div className="fr-card__footer">
-          <ul className="fr-btns-group fr-btns-group--icon-left fr-btns-group--inline-reverse fr-btns-group--inline-lg">
+          <hr/>
+          <ul className="fr-btns-group fr-btns-group--icon-left fr-btns-group--inline-reverse fr-btns-group--inline-lg fr-grid-row--center">
             {demandesCoselec?.statut === 'en_cours' &&
               <>
                 <li>
-                  <button className="fr-btn fr-btn--secondary" onClick={refusAvenantAjoutPoste}>
+                  <button className="fr-btn fr-btn--secondary" onClick={() => setOpenModalRefus(true)}>
                     Refuser la demande
                   </button>
                 </li>
                 <li>
-                  <button className="fr-btn" onClick={() => setOpenModal(true)}>
+                  <button className="fr-btn" onClick={() => setOpenModalValidation(true)}>
                     Valider la demande
                   </button>
                 </li>
               </>
             }
-            <li className="fr-ml-auto">
-              <div className="fr-grid-row" style={{ alignItems: 'baseline' }}>
-                <a className="fr-btn fr-btn--secondary" href={avenant?.url} target="_blank" rel="noopener noreferrer">
-                  Voir le dossier D&eacute;marche Simplifi&eacute;e
-                </a>
-              </div>
-            </li>
           </ul>
         </div>
       </div>
