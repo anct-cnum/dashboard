12a29885a210bacda3ff824ad73281e1936a0de8
morgan <morgan@fitlab.fr>
2024-04-08 16:50:50 +0200

    ignorer erreur t.onDisconnect

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ if (process.env.REACT_APP_SENTRY_ENABLED === 'true') {
     ignoreErrors: [
       //Extension Safari
       /webkit-masked-url/i,
+      /evaluating 't.onDisconnect'/i,
     ],
   });
 }
