a74c2aca54db489eaeb2f5ac0deac2f85dbda81b
Fabien Mercier <fabien.mercier@octo.com>
2024-07-26 11:54:47 +0200

    On met la bonne base pour Vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, transformWithEsbuild } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '',
+  base: '/',
   plugins: [
     {
       name: 'load+transform-js-files-as-jsx',
