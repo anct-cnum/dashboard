a55f678c92f4a83323a9cb0eb5c7568e682b4e2d
ornella452 <ornella93@outlook.fr>
2025-11-21 16:50:11 +0100

    fix: lint

--- src/components/menuRole/MenuStructure.js
+++ src/components/menuRole/MenuStructure.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function MenuStructure({ onClickMenu, activeMenu, trackEvent }) {
-  const authenticationUser = useSelector(state => state.authentication?.user?.entity?.$id);
   const roleActivated = useSelector(state => state.authentication?.roleActivated);
   const urlSiteVitrine = import.meta.env.VITE_APP_PUBLIC_HOSTNAME;
 
